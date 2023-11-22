5d1df71565c06842cfd05725bba4fa1638ad041e
Bendik Samseth <b.samseth@gmail.com>
2023-11-22 18:25:58 +0100

    remove leftover dbg! statement

--- src/cache.rs
+++ src/cache.rs
@@ -79,7 +79,6 @@ impl Cache {
     }
 
     pub fn cache_input(&self, input: &str) -> Result<()> {
-        dbg!(self.input_cache_file());
         std::fs::write(self.input_cache_file(), input)?;
         Ok(())
     }
